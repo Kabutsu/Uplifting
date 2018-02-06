58ae5958370b19b91f40809ee3ee789107bac186
Sam Law <SamJamesLaw@gmail.com>
2018-02-06 15:51:24 +0000

    Softcoded snapping, and added useful files e.g. fonts

---
+++ Assets/Scripts/Initialize.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initialize : MonoBehaviour {
+
+    [SerializeField]
+    private float bottomOfScreen;
+    [SerializeField]
+    private int noOfFloors;
+
+	// Use this for initialization
+	void Start () {
+
+		for(int i = 0; i < noOfFloors; i++)
+        {
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public float BottomOfScreen()
+    {
+        return bottomOfScreen;
+    }
+
+    public int NoOfFloors()
+    {
+        return noOfFloors;
+    }
+}
