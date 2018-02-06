73bbc529812558938e20b2fa0105780dfaa925a5
Sam Law <SamJamesLaw@gmail.com>
2018-02-06 15:34:40 +0000

    Elevator snaps to floors

--- Assets/Scripts/ElevatorMove.cs
+++ Assets/Scripts/ElevatorMove.cs
@@ -4,23 +4,67 @@ using UnityEngine;
 
 public class ElevatorMove : MonoBehaviour {
 
-    private float speed = 1.0f;
+    [SerializeField]
+    protected float speed;
+
+    [SerializeField]
+    private float marginOfError = 0.85f;
+
+    [SerializeField]
+    private int noOfFloors;
+    [SerializeField]
+    public GameObject[] floors;
+
+    [SerializeField]
+    private float bottomOfScreen = -3.75f;
+    [SerializeField]
+    private float topOfScreen = 3.75f;
+
+    private float elevatorX;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        elevatorX = transform.position.x;
+        floors = new GameObject[noOfFloors];
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        //move elevator up
 		if(Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(new Vector3(0, speed * Time.deltaTime));
+            if(transform.position.y <= topOfScreen) transform.Translate(new Vector3(0, speed * Time.deltaTime));
         }
 
+        //move elevator down
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(new Vector3(0, -(speed * Time.deltaTime)));
+            if(transform.position.y >= bottomOfScreen) transform.Translate(new Vector3(0, -(speed * Time.deltaTime)));
+        }
+
+        //snap the elevator to a floor if it is within a certain distance of the floor
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            foreach(GameObject floor in floors)
+            {
+                if(transform.position.y >= floor.transform.position.y - marginOfError &&
+                    transform.position.y <= floor.transform.position.y + marginOfError)
+                {
+                    StartCoroutine(SnapToFloor(new Vector3(elevatorX, floor.transform.position.y), 1f));
+                    break;
+                }
+            }
+        }
+    }
+
+    //move the elevator to the same position as a floor in a specified time
+    IEnumerator SnapToFloor(Vector3 toPosition, float inTime)
+    {
+        var fromPosition = transform.position;
+        for(var t = 0f; t < 1; t += Time.deltaTime / inTime)
+        {
+            transform.position = Vector3.Lerp(fromPosition, toPosition, t);
+            yield return null;
         }
     }
 }
