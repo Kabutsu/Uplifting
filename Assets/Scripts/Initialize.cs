2ba20a500c52c99e8af80c050b680340b0bbb2b3
Sam Law <SamJamesLaw@gmail.com>
2018-02-06 16:45:23 +0000

    Corridors spawnn as part of script attached to Main Camera

--- Assets/Scripts/Initialize.cs
+++ Assets/Scripts/Initialize.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class Initialize : MonoBehaviour {
 
     [SerializeField]
-    private float bottomOfScreen;
+    private GameObject corridorPrefab;
+
+    [SerializeField]
+    private float lowestPositionOfElevator;
     [SerializeField]
     private int noOfFloors;
 
+    private GameObject[] floors;
+
 	// Use this for initialization
 	void Start () {
+        floors = new GameObject[noOfFloors];
+        
+        float corridorHeight = corridorPrefab.GetComponent<Renderer>().bounds.size.y;
 
+        Vector3 floorPosition = new Vector3(3.82f, lowestPositionOfElevator - corridorHeight);
+
+        //spawn all the floors
 		for(int i = 0; i < noOfFloors; i++)
         {
-
+            floorPosition.y += corridorHeight;
+            floors[i] = Instantiate(corridorPrefab, floorPosition, transform.rotation);
         }
+
+        //give the Elevator all the info it needs from this script
+        GameObject.Find("Elevator").GetComponent<ElevatorMove>().Initialize();
 	}
 	
 	// Update is called once per frame
@@ -25,11 +40,16 @@ public class Initialize : MonoBehaviour {
 
     public float BottomOfScreen()
     {
-        return bottomOfScreen;
+        return lowestPositionOfElevator;
     }
 
     public int NoOfFloors()
     {
         return noOfFloors;
+    }
+
+    public GameObject[] Floors()
+    {
+        return floors;
     }
 }
