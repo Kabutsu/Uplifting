6c24b920bd33927ab41530785575ca86d5726e09
= <tfdm1g15@soton.ac.uk>
2018-02-09 15:43:57 +0000

    Added Text and slider to card prefab

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,24 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour {
 
 	//Animation vars
 	const float CARD_SLIDE_IN_TIME_SECONDS = 0.72f;
-	const float CARD_SEPERATION = -155.0f;
+	const float CARD_SEPERATION = -150.0f;
 	const float CARD_TOP_OFFSET = -75.0f;
+	const float CARD_LEFT_POSITION = 230.5f;
 
 
 	//Position vars
 	private RectTransform rectTrans;
-
 	[SerializeField]
 	private int index;
 
 
+	//Child References
+	[SerializeField]
+	private Slider stressSlider;
+	[SerializeField]
+	private Text nameText;
+	[SerializeField]
+	private Text titleText;
+
+
 	// Use this for initialization
 	void Start () {
+		stressSlider = transform.Find ("Slider").GetComponent<Slider> ();
+		nameText = transform.Find ("Name").GetComponent<Text> ();
+		titleText = transform.Find ("Job").GetComponent<Text> ();
+
+		stressSlider.value = 0;
+		nameText.text = "Name";
+		titleText.text = "Title";
 	}
 
 	public void Initialize(int initialIndex){
@@ -48,7 +65,7 @@ public class Card : MonoBehaviour {
 	}
 
 	IEnumerator ReevaluatePosition () {
-		rectTrans.anchoredPosition = new Vector2 (187.0f, CARD_TOP_OFFSET + (CARD_SEPERATION * (float)index));
+		rectTrans.anchoredPosition = new Vector2 (CARD_LEFT_POSITION, CARD_TOP_OFFSET + (CARD_SEPERATION * (float)index));
 		yield return null;
 	}
 
@@ -56,7 +73,7 @@ public class Card : MonoBehaviour {
 	IEnumerator Enter (int index) {
 
 		Vector2 startPoint = new Vector2 (-222.2f, CARD_TOP_OFFSET + (CARD_SEPERATION * (float)index));
-		Vector2 endPoint = new Vector2 (187.0f, CARD_TOP_OFFSET + (CARD_SEPERATION * (float)index));
+		Vector2 endPoint = new Vector2 (CARD_LEFT_POSITION, CARD_TOP_OFFSET + (CARD_SEPERATION * (float)index));
 		rectTrans.anchoredPosition = startPoint;
 
 		float movementTimeElapsed = 0.0f;
