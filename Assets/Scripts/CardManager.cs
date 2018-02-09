b9297e80a73556a721b8f956478d552ffb49fdd8
moorhouse t.f.d. (tfdm1g15) <tfdm1g15@soton.ac.uk>
2018-02-09 12:20:08 +0000

    Added Cards with contructors

---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour {
+
+	private List<Card> currentCards;
+	public GameObject cardPrefab;
+	public Canvas UICanvas;
+
+	// Use this for initialization
+	void Start () {
+		currentCards = new List<Card> ();
+		ConstructCard ();
+		ConstructCard ();
+		ConstructCard ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	public void dismissCard (Card cardToDismiss){
+		int indexBeingDismissed = cardToDismiss.GetIndex ();
+
+		cardToDismiss.Dismiss ();
+		currentCards.Remove (cardToDismiss);
+
+		foreach (Card c in currentCards.FindAll((c) => {return c.GetIndex() > indexBeingDismissed;})) {
+			c.ShiftUp ();
+		}
+
+	}
+
+	public Card ConstructCard(){
+		GameObject go = GameObject.Instantiate (cardPrefab, UICanvas.transform);
+		Card c = go.GetComponent<Card> ();
+		Debug.Log (currentCards.Count);
+		c.Initialize (currentCards.Count);
+		currentCards.Add (c);
+		return c;
+	}
+		
+}
