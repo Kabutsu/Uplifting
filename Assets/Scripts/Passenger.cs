2a57b49abbdefe9181f28b804d6f94f95255068d
Sam Law <SamJamesLaw@gmail.com>
2018-02-11 16:09:49 +0000

    Passengers spawn into lift and Destroy at their stop

---
+++ Assets/Scripts/Passenger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passenger : MonoBehaviour {
+
+    [SerializeField]
+    private Initialize init;
+    [SerializeField]
+    private ElevatorMove elevator;
+
+    private GameController controller;
+
+    private string passengerName;
+    private string job;
+    private int floor;
+    private int rage;
+
+	// Use this for initialization
+	void Start () {
+        controller = GameObject.Find("Game Controller").GetComponent<GameController>();
+        init = GameObject.Find("Game Controller").GetComponent<Initialize>();
+        elevator = GameObject.Find("Elevator").GetComponent<ElevatorMove>();
+
+        passengerName = NameGenerator.Name();
+        job = NameGenerator.Job();
+        rage = 0;
+
+        do
+        {
+            floor = Random.Range(1, init.NoOfFloors());
+        } while (floor == elevator.GetFloor());
+        Debug.Log(floor);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Spawn()
+    {
+
+    }
+
+    public void NotifyFloor(int floorNo)
+    {
+        if (floorNo == floor) LeaveElevator();
+    }
+
+    private void LeaveElevator()
+    {
+        controller.RemovePassenger(this);
+        Destroy(this.gameObject);
+    }
+}
