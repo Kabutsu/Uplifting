8d293a35f5287ee04081aecbfe2599bb65cb2e76
Sam Law <SamJamesLaw@gmail.com>
2018-02-11 18:39:37 +0000

    Passengers now walk on and off the elevator

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,10 +11,12 @@ public class GameController : MonoBehaviour {
     private ElevatorMove elevator;
 
     List<Passenger> passengers;
+    int positionCount;
 
 	// Use this for initialization
 	void Start () {
         passengers = new List<Passenger>();
+        positionCount = 0;
 	}
 	
 	// Update is called once per frame
@@ -24,9 +26,16 @@ public class GameController : MonoBehaviour {
 
     public void RequestPassenger()
     {
-        GameObject passenger = Instantiate(passengerPrefab, elevator.transform);
-        passenger.GetComponent<Passenger>().Spawn();
-        passengers.Add(passenger.GetComponent<Passenger>());
+        if(passengers.Count < 6)
+        {
+            GameObject passenger = Instantiate(passengerPrefab, elevator.transform);
+
+            passenger.transform.localPosition = new Vector3(6, -0.25f, -1);
+
+            passenger.GetComponent<Passenger>().Spawn();
+            
+            passengers.Add(passenger.GetComponent<Passenger>());
+        }
     }
 
     public void BroadcastFloor(int floorNo)
--- Assets/Scripts/Passenger.cs
+++ Assets/Scripts/Passenger.cs
@@ -15,22 +15,17 @@ public class Passenger : MonoBehaviour {
     private string job;
     private int floor;
     private int rage;
+    private float positionOnLift;
 
 	// Use this for initialization
-	void Start () {
-        controller = GameObject.Find("Game Controller").GetComponent<GameController>();
-        init = GameObject.Find("Game Controller").GetComponent<Initialize>();
+	void Start ()
+    {
         elevator = GameObject.Find("Elevator").GetComponent<ElevatorMove>();
+        controller = GameObject.Find("Game Controller").GetComponent<GameController>();
 
         passengerName = NameGenerator.Name();
         job = NameGenerator.Job();
         rage = 0;
-
-        do
-        {
-            floor = Random.Range(1, init.NoOfFloors());
-        } while (floor == elevator.GetFloor());
-        Debug.Log(floor);
 	}
 	
 	// Update is called once per frame
@@ -40,7 +35,29 @@ public class Passenger : MonoBehaviour {
 
     public void Spawn()
     {
+        positionOnLift = Random.Range(-0.9f, 1.2f);
+        init = GameObject.Find("Game Controller").GetComponent<Initialize>();
 
+        elevator = GameObject.Find("Elevator").GetComponent<ElevatorMove>();
+        do
+        {
+            floor = Random.Range(1, init.NoOfFloors());
+        } while (floor == elevator.GetFloor());
+        elevator.Lock();
+
+        StartCoroutine(MoveToPosition(new Vector3(positionOnLift, -0.25f, -1), 1.6f, false));
+    }
+
+    IEnumerator MoveToPosition(Vector3 toPosition, float inTime, bool destroy)
+    {
+        var fromPosition = transform.localPosition;
+        for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
+        {
+            transform.localPosition = Vector3.Lerp(fromPosition, toPosition, t);
+            yield return null;
+        }
+        elevator.Unlock();
+        if (destroy) Destroy(this.gameObject);
     }
 
     public void NotifyFloor(int floorNo)
@@ -51,6 +68,7 @@ public class Passenger : MonoBehaviour {
     private void LeaveElevator()
     {
         controller.RemovePassenger(this);
-        Destroy(this.gameObject);
+        elevator.Lock();
+        StartCoroutine(MoveToPosition(new Vector3(6, -0.25f, -1), 1.5f, true));
     }
 }
