99edd689eca4d4d6a8823d2bcbf8c0676a8fb899
Sam Law <SamJamesLaw@gmail.com>
2018-02-18 16:42:29 +0000

    Forgot to save some stuff

--- Assets/Scripts/TutorialPassenger.cs
+++ Assets/Scripts/TutorialPassenger.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TutorialPassenger : MonoBehaviour {
+public class TutorialPassenger : Passenger {
 
 	// Use this for initialization
 	void Start () {
