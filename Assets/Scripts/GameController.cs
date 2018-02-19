ba52d4a502922bc6f7bc7d01fada25f787f4a775
Sam Law <SamJamesLaw@gmail.com>
2018-02-19 12:45:31 +0000

    Got to [Barbra_on_lift]

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -66,7 +66,7 @@ public class GameController : MonoBehaviour {
 		}
     }
 
-    public void BroadcastFloor(int floorNo)
+    public virtual void BroadcastFloor(int floorNo)
     {
         List<Passenger> temp_passengers = new List<Passenger>(passengers);
         foreach(Passenger passenger in temp_passengers)
@@ -80,7 +80,7 @@ public class GameController : MonoBehaviour {
         passengers.Remove(passenger);
     }
 
-	public int GetPassengerCount(){return passengers.Count;}
+	public virtual int GetPassengerCount(){return passengers.Count;}
 
 	public void GameOver(){
 		Debug.Break ();
