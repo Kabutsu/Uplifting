456ef526c7ab9a65b65d76ffafe2cb9e5c956ad7
Thomas Fynn Douglas Moorhouse <thomasmoorhouse@hotmail.co.uk>
2018-03-03 14:51:46 +0000

    Added main menu scene

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject scrollingBackground;
+
+    // Update is called once per frame
+    void Update() {
+        scrollingBackground.transform.Translate(Vector3.down * Time.deltaTime);
+    }
+
+    public void StartGame()
+    {
+        StartGame(false);
+    }
+
+    public void StartTutorial()
+    {
+        StartGame(true);
+    }
+
+    private void StartGame(bool includeTutorial)
+    {
+        if (includeTutorial)
+        {
+            throw new System.NotSupportedException();
+        } else
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
