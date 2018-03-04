6d583cbb5816afc02acc8a860f4c0f376f735109
Sam Law <SamJamesLaw@gmail.com>
2018-03-04 17:02:03 +0000

    Added "tryouts" in tutorial and freeze in tutorial

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -119,7 +119,7 @@ public class GameController : MonoBehaviour {
         }
     }
 
-    public void RemovePassenger(Passenger passenger)
+    public virtual void RemovePassenger(Passenger passenger)
     {
         passengers.Remove(passenger);
     }
